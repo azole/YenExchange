440ac8c5f07206b055535afc421f5f42f727f2ab
azole <azolelai@gmail.com>
2015-11-27 13:39:06 +0800

    更新乘除

--- www/js/app.js
+++ www/js/app.js
@@ -14,26 +14,24 @@ exchangeApp.controller('calcCtrl', ['$scope', function ($scope) {
   $scope.from = 0;
   $scope.taxRate = 0.08;
   $scope.op = '';
+  $scope.div = 0;
 
   $scope.reset = function(){
     $scope.from = 0;
     $scope.op = '';
+    $scope.div = 0;
   };
 
   $scope.delete = function(){
     $scope.from = Math.floor($scope.from/10);
-    $scope.op = '';
   };
 
   $scope.addNum = function(n){
-    if($scope.op === '*') {
-      $scope.from = $scope.from * n;  
-    } else if($scope.op === '/') {
-      $scope.from = $scope.from / n;  
+    if($scope.op == '*' || $scope.op == '/') {
+      $scope.div = $scope.div * 10 + n;
     } else {
       $scope.from = $scope.from * 10 + n;
     }
-    $scope.op = '';
   };
 
   $scope.setRate = function(){
@@ -44,12 +42,22 @@ exchangeApp.controller('calcCtrl', ['$scope', function ($scope) {
     $scope.rate = $scope.defaultRate;
     $scipe.setRate();
     $scope.op = '';
+    $scope.div = 0;
   };
 
   $scope.advNum = function(op) {
     $scope.op = op;
   };
 
+  $scope.calculate = function(){
+    if($scope.op === '*') {
+      $scope.from = $scope.from * $scope.div;
+    } else if($scope.op === '/') {
+      $scope.from = $scope.from / $scope.div;  
+    }
+    $scope.op = '';
+    $scope.div = 0;
+  };
 }]);
 
 exchangeApp.filter('percentage', ['$filter', function ($filter) {
